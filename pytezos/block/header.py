b3228e2911c9f8acfc59307d73f13757655e1fb7
m-kus <mz@baking-bad.org>
2021-03-12 19:28:11 +0300

    Linter fix

--- pytezos/block/header.py
+++ pytezos/block/header.py
@@ -1,15 +1,21 @@
 from pprint import pformat
-from typing import Any, Dict, List, Optional
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
 
 import bson  # type: ignore
 
-from pytezos.crypto.key import blake2b_32
-from pytezos.block.forge import forge_protocol_data, forge_block_header, forge_signed_operation
+from pytezos.block.forge import forge_block_header
+from pytezos.block.forge import forge_protocol_data
+from pytezos.block.forge import forge_signed_operation
 from pytezos.context.impl import ExecutionContext  # type: ignore
 from pytezos.context.mixin import ContextMixin  # type: ignore
 from pytezos.crypto.encoding import base58_encode
+from pytezos.crypto.key import blake2b_32
 from pytezos.jupyter import get_class_docstring
-from pytezos.michelson.forge import forge_array, forge_base58
+from pytezos.michelson.forge import forge_array
+from pytezos.michelson.forge import forge_base58
 
 
 class BlockHeader(ContextMixin):
@@ -63,7 +69,7 @@ class BlockHeader(ContextMixin):
 
     @classmethod
     def bake_block(cls, context: ExecutionContext, min_fee: int = 0) -> 'BlockHeader':
-        pending_operations = context.shell.mempool.pending_operations()
+        pending_operations = context.shell.mempool.pending_operations()  # type: ignore
         operations: List[Dict[str, Any]] = [
             op
             for op in pending_operations['applied']
