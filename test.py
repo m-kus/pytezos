eedd8e049f7ed1597f492dda516e412201d0c41c
Lev Gorodetskiy <github@droserasprout.space>
2021-12-29 21:40:46 +0400

    Cleanup

--- test.py
+++
@@ -1,9 +0,0 @@
-from pytezos.michelson.micheline import blind_unpack
-
-packed_micheline = '05020000001e070401000000083631363136313631010000000a37333634363436343634'
-expected_michelson = '{ Elt "61616161" "7364646464" }'
-
-micheline_bytes = bytes.fromhex(packed_micheline)
-michelson = blind_unpack(micheline_bytes)
-
-assert expected_michelson == michelson, (expected_michelson, michelson)
